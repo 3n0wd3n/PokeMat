55b7f7dec097f82893f989c41b998b5942b4911f
Michael Hajný <michael.hajny@precismo.com>
2023-10-14 13:06:14 +0200

    Update Map.jsx

--- pokemat/src/components/Map.jsx
+++ pokemat/src/components/Map.jsx
@@ -1,21 +1,21 @@
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+// import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
-export class MapContainer extends Component {
-  render() {
-    return (
-      <Map google={this.props.google} zoom={14}>
-        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+// export class MapContainer extends Component {
+//   render() {
+//     return (
+//       <Map google={this.props.google} zoom={14}>
+//         <Marker onClick={this.onMarkerClick} name={"Current location"} />
 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-            <h1>{this.state.selectedPlace.name}</h1>
-          </div>
-        </InfoWindow>
-      </Map>
-    );
-  }
-}
+//         <InfoWindow onClose={this.onInfoWindowClose}>
+//           <div>
+//             <h1>{this.state.selectedPlace.name}</h1>
+//           </div>
+//         </InfoWindow>
+//       </Map>
+//     );
+//   }
+// }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyCNAKW9Gw9zta7F0JfWEWNoHfs88s0nv80",
-})(MapContainer);
+// export default GoogleApiWrapper({
+//   apiKey: "AIzaSyCNAKW9Gw9zta7F0JfWEWNoHfs88s0nv80",
+// })(MapContainer);
